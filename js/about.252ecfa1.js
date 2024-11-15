119c3c0195aa5b588bce632cbf2c89d72f42833f
woojung1234 <corall5@naver.com>
2024-11-16 02:41:35 +0900

    Updates

--- js/about.252ecfa1.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkmoviewsd"]=self["webpackChunkmoviewsd"]||[]).push([[594],{603:function(n,u,e){e.r(u),e.d(u,{default:function(){return o}});var s=e(6768);const t={class:"about"};function a(n,u){return(0,s.uX)(),(0,s.CE)("div",t,u[0]||(u[0]=[(0,s.Lk)("h1",null,"This is an about page",-1)]))}var r=e(1241);const c={},i=(0,r.A)(c,[["render",a]]);var o=i}}]);
-//# sourceMappingURL=about.252ecfa1.js.map
