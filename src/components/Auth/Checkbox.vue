c021121146ee073e77beceed7ae213c7a072eb9a
woojung1234 <corall5@naver.com>
2024-11-26 00:41:06 +0900

    Input&CheckForm

---
+++ src/components/Auth/Checkbox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Background />
+    <AuthCard
+        :isLoginVisible="isLoginVisible"
+        @toggle="toggleCard"
+    >
+      <template v-if="isLoginVisible">
+        <LoginForm
+            :email="email"
+            :password="password"
+            :rememberMe="rememberMe"
+            :isLoginFormValid="isLoginFormValid"
+            @login="handleLogin"
+        />
+      </template>
+      <template v-else>
+        <RegisterForm
+            :registerEmail="registerEmail"
+            :registerPassword="registerPassword"
+            :confirmPassword="confirmPassword"
+            :acceptTerms="acceptTerms"
+            :isRegisterFormValid="isRegisterFormValid"
+            @register="handleRegister"
+        />
+      </template>
+    </AuthCard>
+  </div>
+</template>
